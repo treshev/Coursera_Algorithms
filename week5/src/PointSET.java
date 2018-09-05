533e9bf10f60f43409f778df8a83f02732a95203
treshchev <a.treshev@gmail.com>
2018-09-05 16:59:40 +0200

    Start week5. Add templete and test content

---
+++ week5/src/PointSET.java
@@ -0,0 +1,65 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET
+{
+    public PointSET()                               // construct an empty set of points
+    {
+    }
+
+    public boolean isEmpty()                      // is the set empty?
+    {
+        return false;
+    }
+
+    public int size()                         // number of points in the set
+    {
+        return 0;
+    }
+
+    public void insert(Point2D p)              // add the point to the set (if it is not already in the set)
+    {
+        if (p == null)
+        {
+            throw new java.lang.IllegalArgumentException();
+        }
+    }
+
+    public boolean contains(Point2D p)            // does the set contain point p?
+    {
+        if (p == null)
+        {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        return false;
+    }
+
+    public void draw()                         // draw all points to standard draw
+    {
+    }
+
+    public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle (or on the boundary)
+    {
+        if (rect == null)
+        {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        return null;
+    }
+
+    public Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
+    {
+        if (p == null)
+        {
+            throw new java.lang.IllegalArgumentException();
+        }
+        return null;
+    }
+
+    public static void main(String[] args)                  // unit testing of the methods (optional)
+    {
+
+    }
+}
